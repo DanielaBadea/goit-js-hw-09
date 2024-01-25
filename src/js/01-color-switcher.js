64ac0690e810f9c2ab1ed46b790e772914e79364
DanielaBadea <badeadaniella@gmail.com>
2024-01-25 03:48:50 +0200

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,38 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  const btnStart = document.querySelector("button[data-start]");
+  const btnStop = document.querySelector("button[data-stop]");
+  const body =document.querySelector("body");
+  
+
+  const changeBodyColor = () => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+    
+  };
+  let timeID;
+
+  const startChangeColor = () => {
+    timeID = setInterval(changeBodyColor, 1000);
+   console.log(timeID);
+   btnStart.disabled = true;
+   btnStop.disabled = false;
+  };
+
+  const stopChangeColor = () => {
+    clearInterval(timeID);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    // if(clearInterval(timeID) !== 'undefined')
+    // {
+    //     btnStart.disabled = false;
+    // }
+    //   else {
+    //     btnStop.disabled = true;
+    //   }
+  };
+
+  btnStart.addEventListener("click", startChangeColor);
+btnStop.addEventListener("click", stopChangeColor);
